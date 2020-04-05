54939bb5c354947a4a01d9e9281fa81db1351436
Kenta Miki <mickey333ff@gmail.com>
2020-04-05 13:05:55 +0900

    [Fix] add ud detector fincution

--- scripts/detection_hog.py
+++ scripts/detection_hog.py
@@ -10,6 +10,7 @@ from std_msgs.msg import String, Int32MultiArray
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import dlib
+import rospkg
 
 class image_converter:
 
@@ -19,10 +20,15 @@ class image_converter:
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/fixed_camera_rgb/rgb/image_raw",Image,self.callback)
 
-        self.SvmFile = "../human/detector.svm"
-        self.detector = dlib.simple_object_detector(self.SvmFile)
+        self.path = rospkg.RosPack().get_path('cv_neuroud') + '/config/'
+
+        self.human_SvmFile = self.path + "human_detector.svm"
+        self.ud_SvmFile = self.path + "ud_detector.svm"
+        self.human_detector = dlib.simple_object_detector(self.human_SvmFile)
+        self.ud_detector = dlib.simple_object_detector(self.ud_SvmFile)
         self.win_det = dlib.image_window()
-        self.win_det.set_image(self.detector)
+        self.win_det.set_image(self.human_detector)
+        self.win_det.set_image(self.ud_detector)
 
     def callback(self,data):
         try:
@@ -30,30 +36,32 @@ class image_converter:
         except CvBridgeError as e:
             print(e)
 
-        dets = self.detector(self.cv_image)
+        human_dets = self.human_detector(self.cv_image)
+        ud_dets = self.ud_detector(self.cv_image)
 
         array=[]
 
-        for d in dets:
+        for d in human_dets:
             # array.append([int(d.left()), int(d.top()), int(d.right()), int(d.bottom())])
             # array.append(int(d.left()))
             array[len(array):len(array)] = [int(d.left()), int(d.top()), int(d.right()), int(d.bottom())]
-            self.write_rec(d)
-        print (array)
+            self.write_rec(d,0,0,255)
+
+        for d in ud_dets:
+            # array.append([int(d.left()), int(d.top()), int(d.right()), int(d.bottom())])
+            # array.append(int(d.left()))
+            array[len(array):len(array)] = [int(d.left()), int(d.top()), int(d.right()), int(d.bottom())]
+            self.write_rec(d,255,0,0)
 
         cv2.imshow("Image window", self.cv_image)
         cv2.waitKey(3)
 
         array_forPublish = Int32MultiArray(data=array)
         self.pnt_pub.publish(array_forPublish)
-        # try:
-        #     self.image_pub.publish(self.bridge.cv2_to_imgmsg(self.cv_image, "bgr8"))
-        # except CvBridgeError as e:
-        #     print(e)
 
 
-    def write_rec(self, d):
-        cv2.rectangle(self.cv_image, (d.left(), d.top()), (d.right(), d.bottom()), (0, 0, 255), 2)
+    def write_rec(self, d, blue, green, red):
+        cv2.rectangle(self.cv_image, (d.left(), d.top()), (d.right(), d.bottom()), (blue, green, red), 2)
 
 def main(args):
     ic = image_converter()
