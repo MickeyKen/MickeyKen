0d68cc7e10b8c9247136701c9b4e3d0c445fdecb
Kenta Miki <mickey333ff@gmail.com>
2020-04-05 12:20:40 +0900

    [Fix] data topic

--- scripts/pts_to_xyz.py
+++ scripts/pts_to_xyz.py
@@ -16,10 +16,12 @@ class Homograpy:
         pts_src = np.array([[10,460], [600,460], [600,20],[10,20]],np.float32)
         pts_org = np.array([[3, -4],[3, 4],[-3, 4],[-3, -4]],np.float32)
         self.h, self.status = cv2.findHomography(pts_src, pts_org)
-        h = np.linalg.inv(h)
+        self.h = np.linalg.inv(self.h)
 
     def callback(self,data):
-        if (len(data))
+        if data.data:
+            print ("human number :", len(data.data)/4)
+
 
 
 def main(args):
