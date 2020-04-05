60264219cf31918bcff4c8d466865a6a64d2c7da
MickeyKen <mickey333ff@gmail.com>
2020-04-05 09:43:02 +0900

    [Add] pts_to_xyz convert program

---
+++ scripts/pts_to_xyz.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+import numpy as np
+from std_msgs.msg import String, Int32MultiArray
+import cv2
+import math
+
+class Homograpy:
+
+    def __init__(self):
+
+        self.sub = rospy.Subscriber("/human_point_xy",Int32MultiArray,self.callback)
+
+        pts_src = np.array([[10,460], [600,460], [600,20],[10,20]],np.float32)
+        pts_org = np.array([[3, -4],[3, 4],[-3, 4],[-3, -4]],np.float32)
+        self.h, self.status = cv2.findHomography(pts_src, pts_org)
+        h = np.linalg.inv(h)
+
+    def callback(self,data):
+        if (len(data))
+
+
+def main(args):
+    ic = Homograpy()
+    rospy.init_node('pts_to_xyz_node', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+
+
+if __name__ == '__main__':
+    main(sys.argv)
